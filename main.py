e978b5c6276ce00d261b2d18b83ab2e4474746a6
amitmoskovitz <amit.moskovitz@cycode.com>
2025-11-18 15:44:23 +0200

    add requirements.txt

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("foo")
+import requests
+
+class MyClass:
+    def __init__(self, url):
+        self.url = url
+
+    def get_data(self):
+        try:
+            response = requests.get(self.url)
+            if response.status_code == 200:
+                print("Success!")
+                return response.json()
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    my_obj = MyClass("https://api.github.com")
+    my_obj.get_data()
